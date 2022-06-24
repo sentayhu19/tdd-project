741bcbce1ccf32551419881316de51939778f71a
IfzaRasool <ifzarasoolarain@gmail.com>
2022-06-24 13:18:17 +0300

    Test for fizzBuzz

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../solver'
+describe Solver do
+  solver = Solver.new
+
+  describe 'Test fizzbuzz method' do
+    it 'return fizz ' do
+        expect(solver.fizzbuzz(9)).to eq 'fizz'
+      end
+      it 'return buzz ' do
+        expect(solver.fizzbuzz(25)).to eq 'buzz'
+      end
+      it 'return fizzbuzz ' do
+        expect(solver.fizzbuzz(15)).to eq 'fizzbuzz'
+      end
+      it 'return Number ' do
+        expect(solver.fizzbuzz(7)).to eq '7'
+      end
+   
+end
